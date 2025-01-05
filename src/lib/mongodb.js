c2d8124f6a363290f183c08af2ab49654e070cfa
Sergiy Horodetskiy <bryklin.best.1994@gmail.com>
2025-01-05 02:05:19 +0200

    mondodb

---
+++ src/lib/mongodb.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+const DB_URI = process.env.DB_URI;
+
+mongoose
+    .connect(DB_URI)
+    .then(() => {
+        console.log('Database connection successful');
+    })
+    .catch(err => {
+        console.log(err);
+        process.exit(1);
+    });
