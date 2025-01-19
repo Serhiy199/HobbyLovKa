6f8f563433a3864251ac1dfa16fa82035ab285b0
Sergiy Horodetskiy <bryklin.best.1994@gmail.com>
2025-01-20 00:23:11 +0200

    руфактор

--- src/lib/mongoDB/mongodb.ts
+++ src/lib/mongoDB/mongodb.ts
@@ -1,19 +1,15 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
-// const DB_URI: string | undefined = process.env.DB_URI;
+const DB_URI: string | undefined = process.env.DB_URI;
 
-// if (!DB_URI) {
-//     console.error('DB_URI is not defined in environment variables');
-//     process.exit(1);
-// }
-
-// const DB_URI = process.env.DB_URI;
+if (!DB_URI) {
+    console.error('DB_URI is not defined in environment variables');
+    process.exit(1);
+}
 
 mongoose
-    .connect(
-        'mongodb+srv://Sergii:1234567890Xxx@cluster0.wa4ou.mongodb.net/db_handBags?retryWrites=true&w=majority&appName=Cluster0'
-    )
+    .connect(DB_URI)
     .then(() => {
         console.log('Database connection successful');
     })
