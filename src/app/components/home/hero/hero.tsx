e33a51d6e8747d0e2a3d94679b70501a51d6f3d7
Sergiy Horodetskiy <bryklin.best.1994@gmail.com>
2025-01-24 01:02:23 +0200

    розбив секцію героя на дві та додав секцію з посиланнями на соц мережі

--- src/app/components/home/hero/hero.tsx
+++ src/app/components/home/hero/hero.tsx
@@ -1,12 +1,23 @@
 import css from './hero.module.css';
+// import '../../../globals.css';
 // import Link from 'next/link';
 
 export default function HeroPage() {
     return (
         <section className={css.hero}>
-            <h1 className={css.title}>Bags of your dreams</h1>
-
-            <h3 className={css.text}>Тут Ви знайдете все, що забажаєте</h3>
+            <div className={css.container}>
+                <h1 className={css.title}>Bags of your dreams</h1>
+                <h3 className={css.text}>Тут Ви знайдете все, що забажаєте</h3>
+            </div>
+            <div className={css.socialLink}>
+                <ul className={css.socialLinkList}>
+                    <h3 className={css.socialLinkTitle}>
+                        Приєднуйся до наших соціальних мереж та будь першим хто побачить новий товар
+                    </h3>
+                    <li>файсбук</li>
+                    <li>інстаграм</li>
+                </ul>
+            </div>
         </section>
     );
 }
