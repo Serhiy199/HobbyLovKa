6f2dda1c324bbf71fbaa6c6119e39219ffb87cf9
Sergiy Horodetskiy <bryklin.best.1994@gmail.com>
2025-01-26 14:41:42 +0200

    додав компонент акції до секції героя

---
+++ src/app/components/home/actions/actions.tsx
@@ -0,0 +1,10 @@
+import css from './actions.module.css';
+
+export default function Actions() {
+    return (
+        <div className={css.container}>
+            <h3 className={css.title}>Акції</h3>
+            <p></p>
+        </div>
+    );
+}
--- src/app/components/home/hero/hero.tsx
+++ src/app/components/home/hero/hero.tsx
@@ -3,6 +3,7 @@ import css from './hero.module.css';
 // import Link from 'next/link';
 // import Image from 'next/image';
 import SocialLink from '../../socialLink/social-link';
+import Actions from '../actions/actions';
 
 export default function HeroPage() {
     return (
@@ -11,11 +12,14 @@ export default function HeroPage() {
                 <h1 className={css.title}>Bags of your dreams</h1>
                 <h3 className={css.text}>Тут Ви знайдете все, що забажаєте</h3>
             </div>
-            <div className={css.socialLink}>
-                <h3 className={css.socialLinkTitle}>
-                    Приєднуйся до наших соціальних мереж та дізнайтесь першим про новий товар
-                </h3>
-                <SocialLink />
+            <div className={css.box}>
+                <div className={css.socialLink}>
+                    <h3 className={css.socialLinkTitle}>
+                        Приєднуйся до наших соціальних мереж та дізнайтесь першим про новий товар
+                    </h3>
+                    <SocialLink />
+                </div>
+                <Actions />
             </div>
         </section>
     );
