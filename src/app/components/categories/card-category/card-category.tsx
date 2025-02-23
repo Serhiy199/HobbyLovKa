480753dbe2313019513645dba64b889c497eae5e
Serhiy Horodetskiy <bryklin.best.1994@gmail.com>
2025-02-24 00:51:51 +0200

    налаштував картку категорї на мобілку планшет та ноутбук

--- src/app/components/categories/card-category/card-category.tsx
+++ src/app/components/categories/card-category/card-category.tsx
@@ -28,11 +28,11 @@ export default function CardCategory({
                                     // fill={true}
                                     style={{
                                         width: '100%',
-                                        // height: 'auto',
+                                        height: 'auto',
                                         maxHeight: '350px',
                                         // maxHeight: '350px',
-                                        // objectFit: 'fill',
-                                        fill: 'true',
+                                        objectFit: 'cover',
+                                        // fill: 'true',
                                     }}
                                     // objectFit="cover"
                                     src={photo}
@@ -46,26 +46,26 @@ export default function CardCategory({
                         </div>
                         <div className={css.back}>
                             <Image
-                                layout="fill"
-                                // width={884}
-                                // height={1280}
-                                fill={true}
+                                // layout="fill"
+                                // objectFit="cover"
+                                width={884}
+                                height={1280}
+                                // fill={true}
                                 // width={242}
                                 // height={242}
-                                style={
-                                    {
-                                        // clipPath:
-                                        //     'polygon(0% 0%, 100% 0%, 90% 50%, 100% 100%, 0% 100%)',
-                                        // height: 'auto',
-                                        // minHeight: '100%',
-                                        // objectFit: 'cover',
-                                        // width: '100%',
-                                        // width: '100%',
-                                        // // height: '242px',
-                                        // maxHeight: '350px',
-                                        // objectFit: 'cover',
-                                    }
-                                }
+                                style={{
+                                    clipPath:
+                                        'polygon(0% 0%, 100% 0%, 90% 50%, 100% 100%, 0% 100%)',
+                                    // height: 'auto',
+                                    // minHeight: '100%',
+                                    maxHeight: '242px',
+                                    objectFit: 'cover',
+                                    width: '100%',
+                                    // width: '100%',
+                                    // // height: '242px',
+                                    // maxHeight: '350px',
+                                    // objectFit: 'cover',
+                                }}
                                 src="/photo_шопер_котик.jpg"
                                 alt={description}
                                 className={css.videoContainer}
