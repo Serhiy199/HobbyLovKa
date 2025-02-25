2bdcbdeb2cf19edbe5671b0d1002046e8ca2f33c
Serhiy Horodetskiy <bryklin.best.1994@gmail.com>
2025-02-25 23:43:57 +0200

    почав робити спливаюче меню на моб

--- src/app/components/header/burger-menu/burger-menu.tsx
+++ src/app/components/header/burger-menu/burger-menu.tsx
@@ -1,8 +1,13 @@
+'use client';
 import css from './burger-menu.module.css';
+
+const handleClick = () => {
+    console.log('click');
+};
 
 export default function BurgerMenu() {
     return (
-        <button type="button" className={css.burger} title="Menu">
+        <button onClick={handleClick} type="button" className={css.burger} title="Menu">
             <svg className={css.icon} width={40} height={40}>
                 <use href="./icons.svg#mob-icon-menu"></use>
             </svg>
