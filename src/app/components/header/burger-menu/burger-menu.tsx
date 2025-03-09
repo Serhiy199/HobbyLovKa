5cce8732ddcd454b7a4a7c8a6a6a6b4ae374bc81
Serhiy Horodetskiy <bryklin.best.1994@gmail.com>
2025-03-09 23:09:33 +0200

    моб меню

--- src/app/components/header/burger-menu/burger-menu.tsx
+++ src/app/components/header/burger-menu/burger-menu.tsx
@@ -11,7 +11,7 @@ import MobileCategory from './mobile-category/mobile-category';
 
 export default function BurgerMenu() {
     // const pathname: string = usePathname();
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
 
     const handleClickOpen = () => {
         setIsOpen(true);
@@ -27,39 +27,51 @@ export default function BurgerMenu() {
                     <use href="./icons.svg#mob-icon-menu"></use>
                 </svg>
             </button>
-            <div className={clsx(css.mobileMenu, isOpen && css.isOpen)}>
-                <div className={css.boxHead}>
-                    <Logo textColor={'mobile-menu'} />
-                    <button
-                        onClick={handleClickClose}
-                        type="button"
-                        className={css.closeBtn}
-                        title="Close menu"
-                    >
-                        <RiCloseLargeLine className={css.closeIcon} />
-                    </button>
+            {isOpen && (
+                <div className={css.mobileMenu}>
+                    <div className={css.mobileMenuContainer}>
+                        <div className={css.boxHead}>
+                            <Logo textColor={'mobile-menu'} />
+                            <button
+                                onClick={handleClickClose}
+                                type="button"
+                                className={css.closeBtn}
+                                title="Close menu"
+                            >
+                                <RiCloseLargeLine className={css.closeIcon} />
+                            </button>
+                        </div>
+                        <MobileCategory onClickClose={setIsOpen} />
+                        <div className={css.boxContact}>
+                            <ul className={css.contact}>
+                                <li className={css.contactItem}>
+                                    <svg
+                                        className={css.contactsContMail}
+                                        width="16px"
+                                        height="12px"
+                                    >
+                                        <use href="/icons.svg#icon-email"></use>
+                                    </svg>
+                                    <Link href="mailto:maria.horog.89@gmail.com">
+                                        maria.horog.89@gmail.com
+                                    </Link>
+                                </li>
+                                <li className={css.contactItem}>
+                                    <svg
+                                        className={css.contactsContMail}
+                                        width="10px"
+                                        height="16px"
+                                    >
+                                        <use href="/icons.svg#smartphone"></use>
+                                    </svg>
+                                    <Link href="tel:+380961050086"> +38 096 105 00 86</Link>
+                                </li>
+                            </ul>
+                            <SocialLink />
+                        </div>
+                    </div>
                 </div>
-                <MobileCategory />
-                <div className={css.boxContact}>
-                    <ul className={css.contact}>
-                        <li className={css.contactItem}>
-                            <svg className={css.contactsContMail} width="16px" height="12px">
-                                <use href="/icons.svg#icon-email"></use>
-                            </svg>
-                            <Link href="mailto:maria.horog.89@gmail.com">
-                                maria.horog.89@gmail.com
-                            </Link>
-                        </li>
-                        <li className={css.contactItem}>
-                            <svg className={css.contactsContMail} width="10px" height="16px">
-                                <use href="/icons.svg#smartphone"></use>
-                            </svg>
-                            <Link href="tel:+380961050086"> +38 096 105 00 86</Link>
-                        </li>
-                    </ul>
-                    <SocialLink />
-                </div>
-            </div>
+            )}
         </>
     );
 }
--- src/app/components/header/burger-menu/mobile-category/mobile-category.tsx
+++ src/app/components/header/burger-menu/mobile-category/mobile-category.tsx
@@ -5,78 +5,91 @@ import { usePathname } from 'next/navigation';
 import css from './mobile-category.module.css';
 import clsx from 'clsx';
 
-export default function MobileCategory() {
+interface Props {
+    onClickClose: (isOpen: boolean) => void;
+}
+
+export default function MobileCategory({ onClickClose }: Props) {
     const pathname: string = usePathname();
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
 
     const handleClickOpen = () => {
         setIsOpen(true);
     };
-    // const handleClickClose = () => {
-    //     setIsOpen(false);
-    // };
+    const handleClickClose = () => {
+        onClickClose(false);
+    };
 
     return (
         <nav className={css.navigation}>
-            <ul onClick={handleClickOpen} className={clsx(css.list, isOpen && css.isClose)}>
-                <li className={css.item}>
-                    <Link href="/" className={clsx(css.menuLink, pathname === '/' && css.isActive)}>
-                        Головна
-                    </Link>
-                </li>
-                <li className={css.item}>
-                    <Link href="/#category" className={css.menuLinkCategory}>
-                        Категорій
-                    </Link>
-                </li>
-                <li className={css.item}>
-                    <Link
-                        href="/catalog"
-                        className={clsx(css.menuLink, pathname === '/catalog' && css.isActive)}
-                    >
-                        Каталог товарів
-                    </Link>
-                </li>
-            </ul>
+            {!isOpen && (
+                <ul className={css.list}>
+                    <li className={css.item}>
+                        <Link
+                            href="/"
+                            onClick={handleClickClose}
+                            className={clsx(css.menuLink, pathname === '/' && css.isActive)}
+                        >
+                            Головна
+                        </Link>
+                    </li>
+                    <li className={css.item}>
+                        <Link href="" onClick={handleClickOpen} className={css.menuLinkCategory}>
+                            Категорій
+                        </Link>
+                    </li>
+                    <li className={css.item}>
+                        <Link
+                            href="/catalog"
+                            onClick={handleClickClose}
+                            className={clsx(css.menuLink, pathname === '/catalog' && css.isActive)}
+                        >
+                            Каталог товарів
+                        </Link>
+                    </li>
+                </ul>
+            )}
 
-            <ul className={clsx(css.isClose, isOpen && css.isOpen)}>
-                <li className={css.item}>
-                    <Link href={'/catalog'} className={css.link}>
-                        Готові вироби
-                    </Link>
-                </li>
-                <li className={css.item}>
-                    <Link href={'/catalog'} className={css.link}>
-                        Схеми для вишивки бісером
-                    </Link>
-                </li>
+            {isOpen && (
+                <ul className={css.category}>
+                    <li className={css.item}>
+                        <Link href={'/catalog'} onClick={handleClickClose} className={css.link}>
+                            Готові вироби
+                        </Link>
+                    </li>
+                    <li className={css.item}>
+                        <Link href={'/catalog'} onClick={handleClickClose} className={css.link}>
+                            Схеми для вишивки бісером
+                        </Link>
+                    </li>
 
-                <li className={css.item}>
-                    <Link href={'/catalog'} className={css.link}>
-                        Інструменти для рукоділля
-                    </Link>
-                </li>
-                <li className={css.item}>
-                    <Link href={'/catalog'} className={css.link}>
-                        Сумки
-                    </Link>
-                </li>
-                <li className={css.item}>
-                    <Link href={'/catalog'} className={css.link}>
-                        Аксесуари
-                    </Link>
-                </li>
-                <li className={css.item}>
-                    <Link href={'/catalog'} className={css.link}>
-                        Бісер PRECIOSA
-                    </Link>
-                </li>
-                <li className={css.item}>
-                    <Link href={'/catalog'} className={css.link}>
-                        Біжутерія
-                    </Link>
-                </li>
-            </ul>
+                    <li className={css.item}>
+                        <Link href={'/catalog'} onClick={handleClickClose} className={css.link}>
+                            Інструменти для рукоділля
+                        </Link>
+                    </li>
+                    <li className={css.item}>
+                        <Link href={'/catalog'} onClick={handleClickClose} className={css.link}>
+                            Сумки
+                        </Link>
+                    </li>
+                    <li className={css.item}>
+                        <Link href={'/catalog'} onClick={handleClickClose} className={css.link}>
+                            Аксесуари
+                        </Link>
+                    </li>
+                    <li className={css.item}>
+                        <Link href={'/catalog'} onClick={handleClickClose} className={css.link}>
+                            Бісер PRECIOSA
+                        </Link>
+                    </li>
+                    <li className={css.item}>
+                        <Link href={'/catalog'} onClick={handleClickClose} className={css.link}>
+                            Біжутерія
+                        </Link>
+                    </li>
+                </ul>
+            )}
         </nav>
     );
 }
